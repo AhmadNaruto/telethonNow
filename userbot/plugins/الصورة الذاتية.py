b73d9938f9fffcd67c01ccad174b471724df5176
mustafaalazaw <90499028+mustafaalazaw@users.noreply.github.com>
2021-09-23 20:33:17 +0300

    Create الصورة الذاتية.py

---
+++ userbot/plugins/الصورة الذاتية.py
@@ -0,0 +1,22 @@
+from userbot import iqthon
+
+from . import *
+
+
+@iqthon.iq(admin_cmd(pattern="جلب الصورة"))
+async def oho(event):
+    if not event.is_reply:
+        return await event.edit("يجـب عـليك الـرد عـلى صـورة ذاتيـة الـتدمير")
+    mustafa = await event.get_reply_message()
+    pic = await mustafa.download_media()
+    await bot.send_file(
+        "me",
+        pic,
+        caption=f"""
+-تـم جـلب الصـورة بنجـاح ✅
+
+  """,
+    )
+    await event.delete()
+
+
