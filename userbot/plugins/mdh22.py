98e93fabd6919afb997d80cca47c72952e75f0e1
mustafaalazaw <90499028+mustafaalazaw@users.noreply.github.com>
2021-10-02 18:32:30 +0300

    Update mdh22.py

--- userbot/plugins/mdh22.py
+++ userbot/plugins/mdh22.py
@@ -5,22 +5,21 @@ from . import ALIVE_NAME, StartTime, get_readable_time, mention, reply_id
 DEFULTUSER = ALIVE_NAME or "IQTHON"
 IMOGTHON = "**ٍَ 🖤 ❬**"
 
-
 @iqthon.on(admin_cmd(outgoing=True, pattern="المده$"))
 @iqthon.on(sudo_cmd(pattern="المده$", allow_sudo=True))
 async def uptics(ics):
     if ics.fwd_from:
         return
-    iqtid = await reply_id(ics)
-    iqtupt = await get_readable_time((time.time() - StartTime))
-    if iqt_IMG:
-        iqt_c += f"**{IMOGTHON} المـسـتـخـدم :** {mention}:   ٍَ❭\n"
-        iqt_c += f"**{IMOGTHON} مـدة الـتـشغـيـل :** `{iqtupt}`:   ٍَ❭"
-        await ics.client.send_file(ics.chat_id, iqt_IMG, caption=iqt_c, reply_to=iqtid)
+    icsid = await reply_id(ics)
+    icsupt = await get_readable_time((time.time() - StartTime))
+    if ICS_IMG:
+        ics_c += f"**{IMOGTHON} المستخدم :** {mention}\n"
+        ics_c += f"**{IMOGTHON} مدة التشغيل :** `{icsupt}`"
+        await ics.client.send_file(ics.chat_id, ICS_IMG, caption=ics_c, reply_to=icsid)
         await ics.delete()
     else:
         await edit_or_reply(
             ics,
-            f"**{IMOGTHON} المـسـتـخـدم :** {mention}:   ٍَ❭\n"
-            f"**{IMOGTHON} مـدة الـتـشغـيـل :** `{iqtupt}`:   ٍَ❭",
+            f"**{IMOGTHON} المستخدم :** {mention}\n"
+            f"**{IMOGTHON} مدة التشغيل :** `{icsupt}`",
         )
