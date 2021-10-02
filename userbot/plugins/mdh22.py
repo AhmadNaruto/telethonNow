35160932b36dc9e51e9cb1a45c0e0287c2d316c5
mustafaalazaw <90499028+mustafaalazaw@users.noreply.github.com>
2021-10-02 21:45:26 +0300

    Update mdh22.py

--- userbot/plugins/mdh22.py
+++ userbot/plugins/mdh22.py
@@ -1,30 +1,68 @@
+import random
+import re
 import time
-from userbot import iqthon
-from . import ALIVE_NAME, StartTime, get_readable_time, mention, reply_id
+from platform import python_version
 
-DEFULTUSER = ALIVE_NAME or "IQTHONbot"
-IQT_IMG = "https://telegra.ph/file/17f2ad9df0b5aeed779d1.mp4"
-IQTS_TEXT = "𓆩 𝑾𝑬𝑳𝑪𝑶𝑴𝑬  :"
-IQTEM = "**⌔ :**"
+from telethon import version
+from telethon.errors.rpcerrorlist import (
+    MediaEmptyError,
+    WebpageCurlFailedError,
+    WebpageMediaEmptyError,
+)
+from telethon.events import CallbackQuery
+
+from userbot import StartTime, iqthon, catversion
+
+from ..Config import Config
+from ..core.managers import edit_or_reply
+from ..helpers.functions import catalive, check_data_base_heal_th, get_readable_time
+from ..helpers.utils import reply_id
+from ..sql_helper.globals import gvarstatus
+from . import mention
+
+plugin_category = "utils"
 
 
-@iqthon.on(admin_cmd(outgoing=True, pattern="المده$"))
-@iqthon.on(sudo_cmd(pattern="المده$", allow_sudo=True))
-async def uptics(IQT):
-    if IQT.fwd_from:
-        return
-    IQTid = await reply_id(IQT)
-    IQTupt = await get_readable_time((time.time() - StartTime))
-    if IQT_IMG:
-        IQT_c = f"**{IQTS_TEXT}**\n"
-        IQT_c += f"**{IQTEM} المستخدم :** {mention}\n"
-        IQT_c += f"**{IQTEM} مدة التشغيل :** `{IQTupt}`"
-        await IQT.client.send_file(IQT.chat_id, IQT_IMG, caption=IQT_c, reply_to=IQTid)
-        await IQT.delete()
+@iqthon.iq_cmd(
+    pattern="المده$",
+    command=("المده", plugin_category),
+    info={
+        "header": "To check bot's alive status",
+        "options": "To show media in this cmd you need to set ALIVE_PIC with media link, get this by replying the media by .tgm",
+        "usage": [
+            "{tr}المده",
+        ],
+    },
+)
+async def amireallyalive(event):
+    "A kind of showing bot details"
+    reply_to_id = await reply_id(event)
+    uptime = await get_readable_time((time.time() - StartTime))
+    _, check_sgnirts = check_data_base_heal_th()
+    EMOJI_TELETHON = gvarstatus("ALIVE_EMOJI") or " ٍَ 🖤"
+    IQTHON_ALIVE_TEXT = "❬ تـليثون العـرب - Telethon-Arabe ، 🕸  ❭ :"
+    IQTHON_IMG = gvarstatus("ALIVE_PIC")
+    if IQTHON_IMG:
+        CAT = [x for x in IQTHON_IMG.split()]
+        A_IMG = list(CAT)
+        PIC = random.choice(A_IMG)
+        cat_caption = f"**{IQTHON_ALIVE_TEXT}**\n"
+        cat_caption += f"**{EMOJI_TELETHON} ❬ ِحسـابك  :   {mention}  ٍ ٍَ❭**\n"
+        cat_caption += f"**{EMOJI_TELETHON}❬ ٰمـدة الـتشغيل  : {uptime}  ٍَ❭**"
+        try:
+            await event.client.send_file(
+                event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
+            )
+            await event.delete()
+        except (WebpageMediaEmptyError, MediaEmptyError, WebpageCurlFailedError):
+            return await edit_or_reply(
+                event,
+                f"**مدة التشغيل",
+            )
     else:
         await edit_or_reply(
-            IQT,
-            f"**{IQTS_TEXT}**\n\n"
-            f"**{IQTEM} المستخدم :** {mention}\n"
-            f"**{IQTEM} مدة التشغيل :** `{IQTupt}`",
+            event,
+            f"**{IQTHON_ALIVE_TEXT}**\n"
+            f"**{EMOJI_TELETHON} ❬ ِحسـابك  :   {mention}  ٍ ٍَ❭**\n"
+            f"**{EMOJI_TELETHON}❬ ٰمـدة الـتشغيل  : {uptime}  ٍَ❭**"
         )
