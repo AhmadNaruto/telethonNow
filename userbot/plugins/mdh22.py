cd84d4eeaa62fd22c5238daa0dc9acbd5f4d9dcb
mustafaalazaw <90499028+mustafaalazaw@users.noreply.github.com>
2021-10-02 18:56:23 +0300

    Update mdh22.py

--- userbot/plugins/mdh22.py
+++ userbot/plugins/mdh22.py
@@ -1,25 +1,22 @@
 import time
 from userbot import iqthon
-from . import ALIVE_NAME, StartTime, get_readable_time, mention, reply_id
 
-DEFULTUSER = ALIVE_NAME or "IQTHON"
-IMOGTHON = "**ٍَ 🖤 ❬**"
 
-@iqthon.on(admin_cmd(outgoing=True, pattern="المده$"))
-@iqthon.on(sudo_cmd(pattern="المده$", allow_sudo=True))
-async def uptics(ics):
-    if ics.fwd_from:
-        return
-    icsid = await reply_id(ics)
-    icsupt = await get_readable_time((time.time() - StartTime))
-    if ICS_IMG:
-        ics_c += f"**{IMOGTHON} المستخدم :** {mention}\n"
-        ics_c += f"**{IMOGTHON} مدة التشغيل :** `{icsupt}`"
-        await ics.client.send_file(ics.chat_id, ICS_IMG, caption=ics_c, reply_to=icsid)
-        await ics.delete()
-    else:
-        await edit_or_reply(
-            ics,
-            f"**{IMOGTHON} المستخدم :** {mention}\n"
-            f"**{IMOGTHON} مدة التشغيل :** `{icsupt}`",
-        )
+from . import StartTime, mention
+from . import get_readable_time as grt
+
+@iqthon.on(
+    iqthon_cmd(
+       pattern="مده", outgoing=True
+    )
+)
+@iqthon.on(
+    sudo_cmd(
+       pattern="مده", allow_sudo=True
+    )
+)
+async def tim(kon):
+    timethon = await grt((time.time() - StartTime))
+    await eor(
+        kon, f"⌔∮ مستخدم البوت : \n  - {mention} \n⌔∮ مدة تشغيل البوت : \n  - {timethon}"
+    )
