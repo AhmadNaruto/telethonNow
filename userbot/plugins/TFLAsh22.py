cd8a47e73a7893e49c29c003b89dde50e9b8de39
mustafaalazaw <90499028+mustafaalazaw@users.noreply.github.com>
2021-10-05 03:20:47 +0300

    Update TFLAsh22.py

--- userbot/plugins/TFLAsh22.py
+++ userbot/plugins/TFLAsh22.py
@@ -21,7 +21,7 @@ from telethon.tl.types import (
 )
 
 from userbot import iqthon
-from userbot.utils import admin_cmd, sudo_cmd, eor
+from userbot.utils import ban_user
 from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
 from ..helpers import readable_time
