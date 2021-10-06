ea4e61808f4fb2cc62d5598e3adbbcfd5074284f
mustafaalazaw <90499028+mustafaalazaw@users.noreply.github.com>
2021-10-07 02:48:47 +0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -2,7 +2,7 @@ import sys
 
 import userbot
 from userbot import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
-from telethon.tl.functions.channels import JoinChannelRequest
+from telethon import functions
 
 from .Config import Config
 from .core.logger import logging
