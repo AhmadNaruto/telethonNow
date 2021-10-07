8282e70bac614417272394b1823e98da425d9002
mustafaalazaw <90499028+mustafaalazaw@users.noreply.github.com>
2021-10-07 18:37:00 +0300

    Update xobot22.py

--- userbot/plugins/xobot22.py
+++ userbot/plugins/xobot22.py
@@ -42,13 +42,13 @@ async def nope(doit):
 
 @iqthon.on(admin_cmd(pattern="همسه ?(.*)"
                    ))
-async def wspr(event):
+async def hmsh(event):
     if event.fwd_from:
         return
     wwwspr = event.pattern_match.group(1)
-    bu = "@nnbbot"
+    botusername = "@whisperBot"
     if event.reply_to_msg_id:
-        reply_to_id = await event.get_reply_message()
-    tap = await bot.inline_query(bu, wwwspr) 
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, wwwspr)
     await tap[0].click(event.chat_id)
     await event.delete()
