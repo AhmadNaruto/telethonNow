3705cabeeb4f435914d867a35c574cf7506a5d03
mustafaalazaw <90499028+mustafaalazaw@users.noreply.github.com>
2021-10-07 17:52:20 +0300

    Update unread.py

--- userbot/plugins/unread.py
+++ userbot/plugins/unread.py
@@ -7,13 +7,13 @@ from pyrogram.types import Message
 from userbot import iqthon
 
 
-@Iqthon.on_message(filters.command(["unread", "un"], ".") & filters.me)
+@UserBot.on_message(filters.command(["unread", "un"], ".") & filters.me)
 async def mark_chat_unread(bot, message: Message):
     await asyncio.gather(
         message.delete(),
         bot.send(
             functions.messages.MarkDialogUnread(
-                peer=await Iqthon.resolve_peer(message.chat.id), unread=True
+                peer=await UserBot.resolve_peer(message.chat.id), unread=True
             )
         ),
     )
