f791f60dca865af1ef8ad5bff3c58c2eff3b0ef8
mustafaalazaw <90499028+mustafaalazaw@users.noreply.github.com>
2021-10-07 03:10:13 +0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -79,7 +79,8 @@ def start_bot():
 check = start_bot()
 if check == False:
     print("لايمكن البدء حتى الاشتراك بالقنوات التاليه في تليجرام @yzzzy - @iqthon - @M4_STORY")
-    iqthon.loop.run_until_complete(setup_bot())               
+                   
+iqthon.loop.run_until_complete(startup_process())
    
 
 if len(sys.argv) not in (1, 3, 4):
