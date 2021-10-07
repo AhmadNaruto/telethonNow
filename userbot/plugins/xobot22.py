b5378fd26044ceeaeb2bf90fa5337b9b792acbc2
mustafaalazaw <90499028+mustafaalazaw@users.noreply.github.com>
2021-10-07 19:26:42 +0300

    Update xobot22.py

--- userbot/plugins/xobot22.py
+++ userbot/plugins/xobot22.py
@@ -1,6 +1,8 @@
 import re
 
 from userbot import iqthon
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 
 IQMOG = re.compile(
@@ -52,3 +54,20 @@ async def hmsh(SLQ):
     l5 = await bot.inline_query(donttag, kkno)
     await l5[0].click(SLQ.chat_id)
     await SLQ.delete()
+
+@iqthon.on(admin_cmd(pattern="فحص الحظر ?(.*)")
+async def spam(SLQ):
+    await event.edit("`Processing...`")
+    async with bot.conversation("@SpamBot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=178220800)
+            )
+            await conv.send_message("/start")
+            response = await response
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.edit("**Mohon Unblock @SpamBot dan coba lagi**")
+            return
+        await event.edit(f"~ {response.message.message}")    
+    
