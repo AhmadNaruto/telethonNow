f2ce120e5fa117c3cd82543e4a00d8f325c45e4e
mustafaalazaw <90499028+mustafaalazaw@users.noreply.github.com>
2021-10-08 14:44:19 +0300

    Create spampic22.py

---
+++ userbot/plugins/spampic22.py
@@ -0,0 +1,22 @@
+import time
+import threading
+
+from re import sub
+from telethon import events
+
+from asyncio import wait, sleep
+
+from userbot import iqthon
+
+
+@iqthon.on(admin_cmd(pattern="تكرار صور (?: |$)(.*)"))
+async def tiny_pic_spam(e):
+    arr = extract_args_arr(e)
+    if len(arr) < 2 or not arr[0].isdigit():
+        await e.edit("هناك خطا")
+        return
+    await e.delete()
+    counter = int(arr[0])
+    link = arr[1]
+    for i in range(0, counter):
+        await e.client.send_file(e.chat_id, link)
