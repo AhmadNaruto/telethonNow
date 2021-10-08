75cde80f16634f94bf18243e9b3cd8f67f6b566b
mustafaalazaw <90499028+mustafaalazaw@users.noreply.github.com>
2021-10-08 21:07:29 +0300

    Update بروفايل وقتي.py

--- userbot/plugins/بروفايل وقتي.py
+++ userbot/plugins/بروفايل وقتي.py
@@ -41,12 +41,11 @@ DEFAULTUSER = AUTONAME or Config.ALIVE_NAME
 LOGS = logging.getLogger(__name__)
 
 FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
-name1 = first_name
 autopic_path = os.path.join(os.getcwd(), "userbot", "original_pic.png")
 digitalpic_path = os.path.join(os.getcwd(), "userbot", "digital_pic.png")
 autophoto_path = os.path.join(os.getcwd(), "userbot", "photo_pfp.png")
 EMOJI_TELETHON = gvarstatus("ALIVE_EMOJI") or "•"
-
+name1 = first_name
 digitalpfp = Config.DIGITAL_PIC or "https://telegra.ph/file/6b5db91f38e919e386168.jpg"
 
 COLLECTION_STRINGS = {
