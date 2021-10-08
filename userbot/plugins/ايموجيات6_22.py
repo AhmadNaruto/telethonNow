faa6ff6c7eeeb2c97e7b24a759c40c17c9320684
mustafaalazaw <90499028+mustafaalazaw@users.noreply.github.com>
2021-10-08 03:55:36 +0300

    Create ايموجيات6_22.py

---
+++ userbot/plugins/ايموجيات6_22.py
@@ -0,0 +1,162 @@
+import asyncio
+from time import sleep
+from telethon import events
+
+from userbot import iqthon
+
+@iqthon.on(admin_cmd(pattern="تفاعلات(?: |$)(.*)"))
+async def koc(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("أ‿أ")
+        await e.edit("╥﹏╥")
+        await e.edit("(;﹏;)")
+        await e.edit("(ToT)")
+        await e.edit("(┳Д┳)")
+        await e.edit("(ಥ﹏ಥ)")
+        await e.edit("（；へ：）")
+        await e.edit("(T＿T)")
+        await e.edit("（πーπ）")
+        await e.edit("(Ｔ▽Ｔ)")
+        await e.edit("(⋟﹏⋞)")
+        await e.edit("（ｉДｉ）")
+        await e.edit("(´Д⊂ヽ")
+        await e.edit("(;Д;)")
+        await e.edit("（>﹏<）")
+        await e.edit("(TдT)")
+        await e.edit("(つ﹏⊂)")
+        await e.edit("༼☯﹏☯༽")
+        await e.edit("(ノ﹏ヽ)")
+        await e.edit("(ノAヽ)")
+        await e.edit("(╥_╥)")
+        await e.edit("(T⌓T)")
+        await e.edit("(༎ຶ⌑༎ຶ)")
+        await e.edit("(☍﹏⁰)｡")
+        await e.edit("(ಥ_ʖಥ)")
+        await e.edit("(つд⊂)")
+        await e.edit("(≖͞_≖̥)")
+        await e.edit("(இ﹏இ`｡)")
+        await e.edit("༼ಢ_ಢ༽")
+        await e.edit("༼ ༎ຶ ෴ ༎ຶ༽")
+
+@iqthon.on(admin_cmd(pattern="اخذ قلبي(?: |$)(.*)"))        
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`\n(\\_/)`"
+                     "`\n(●_●)`"
+                     "`\n />❤️ اخذ قلبي")
+    sleep(3)
+    await typew.edit("`\n(\\_/)`"
+                     "`\n(●_●)`"
+                     "`\n/>💔  رجعلياه مكسور")
+    sleep(2)
+    await typew.edit("`\n(\\_/)`"
+                     "`\n(●_●)`"
+                     "`\n💔<\\  خوات كحبه") 
+   
+    
+@iqthon.on(admin_cmd(pattern="اشوفج السطح(?: |$)(.*)"))
+async def _(event):
+    "أمر الرسوم المتحركة"
+    animation_interval = 3
+
+    animation_ttl = range(0, 103)
+    animation_chars = [
+            "  😐             😕 \n/👕\\         <👗\\ \n 👖               /|",
+            "  😉          😳 \n/👕\\       /👗\\ \n  👖            /|",
+            "  😚            😒 \n/👕\\         <👗> \n  👖             /|",
+            "  😍         ☺️ \n/👕\\      /👗\\ \n  👖          /|",
+            "  😍          😍 \n/👕\\       /👗\\ \n  👖           /|",
+            "  😘   😊 \n /👕\\/👗\\ \n   👖   /|",
+            " 😳  😁 \n /|\\ /👙\\ \n /     / |",
+            "😈    /😰\\ \n<|\\      👙 \n /🍆    / |",
+            "😅 \n/(),✊😮 \n /\\         _/\\/|",
+            "😎 \n/\\_,__😫 \n  //    //       \\",
+            "😖 \n/\\_,💦_😋  \n  //         //        \\",
+            "  😭      ☺️ \n  /|\\   /(👶)\\ \n  /!\\   / \\ ",
+            "😅`"]
+    
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 103])      
+
+@iqthon.on(admin_cmd(pattern="احبك(?: |$)(.*)"))            
+async def koc(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("احبك 💕")
+        await e.edit("💝💘💓💗")
+        await e.edit("💞💕💗💘")
+        await e.edit("💝💘💓💗")
+        await e.edit("💞💕💗💘")
+        await e.edit("💘💞💗💕")
+        await e.edit("💘💞💕💗")
+        await e.edit("احبك 💝💖💘")
+        await e.edit("💝💘💓💗")
+        await e.edit("💞💕💗💘")
+        await e.edit("💘💞💕💗")
+        await e.edit("احبك")
+        await e.edit("احبك")
+        await e.edit("احبك 💕")
+        await e.edit("💘💘💘💘")
+        await e.edit("احبك")
+        await e.edit("احبك")
+        await e.edit("احبك")
+        await e.edit("احبك")
+        await e.edit("احبك")
+        await e.edit("احبك")
+        await e.edit("💕💞💘💝")
+        await e.edit("💘💕💞💝")
+        await e.edit("احبك💞")            
+            
+@iqthon.on(admin_cmd(pattern="اركض(?: |$)(.*)"))            
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("اركض")
+    sleep(1)
+    await typew.edit("اركض")
+    sleep(1)
+    await typew.edit("`🏃                        🦖`")
+    await typew.edit("`🏃                       🦖`")
+    await typew.edit("`🏃                      🦖`")
+    await typew.edit("`🏃                     🦖`")
+    await typew.edit("`🏃                    🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                    🦖`")
+    await typew.edit("`🏃                     🦖`")
+    await typew.edit("`🏃                    🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃          🦖`")
+    await typew.edit("`🏃         🦖`")
+    await typew.edit("`🏃       🦖`")
+    await typew.edit("`🏃      🦖`")
+    await typew.edit("`🏃     🦖`")
+    await typew.edit("`🏃    🦖`")
+    await typew.edit("`🧎🦖`")
+
