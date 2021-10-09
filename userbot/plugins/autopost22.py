212e8d9ce94d3d394372364078ac3c137b34a067
mustafaalazaw <90499028+mustafaalazaw@users.noreply.github.com>
2021-10-10 02:55:40 +0300

    Update autopost22.py

--- userbot/plugins/autopost22.py
+++ userbot/plugins/autopost22.py
@@ -37,7 +37,7 @@ async def _(event):
     remove_post(iq, event.chat_id)
     await eor(event, f"**📍 Stopped AutoPosting From** `{hel_}`")
 
-@iqthon.on(events.NewMessage())
+@iqthon.on(admin_cmd(pattern=None())
 async def _(event):
     if event.is_private:
         return
